a3aaee16c33b275fa0167cb2f546335e2f2191fc
NikitaSabne <nikitas@incubxperts.com>
2022-06-21 19:07:21 +0530

    dev-Nikita created table using ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,38 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'directive';
+  rows = [
+    {
+      "Name": "Shikha",
+      "Lastname": "Jaiswal",
+      "Zip": "433676",
+      "Phoneno": "7856029871"
+    },
+    {
+      "Name": "Abhishek",
+      "Lastname": "Maudiraj",
+      "Zip": "687366",
+      "Phoneno": "7856029871"
+    },
+    {
+      "Name": "shambhavi",
+      "Lastname": "Duggal",
+      "Zip": "982565",
+      "Phoneno": "7856029871"
+    },
+    {
+      "Name": "Joysi",
+      "Lastname": "Christien",
+      "Zip": "433676",
+      "Phoneno": "7856029871"
+    },
+    {
+      "Name": "tom",
+      "Lastname": "claon",
+      "Zip": "433676",
+      "Phoneno": "7856029871"
+    }
+  ];
 }
