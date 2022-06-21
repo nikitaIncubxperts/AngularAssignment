f90ef4fd67524ff66ff1a408de8755e5cc36ff16
nikitaIncubxperts <nikitas@incubxperts.com>
2022-06-21 23:17:55 +0530

    dev_Nikita added hide-show button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,40 +3,50 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
+  click = true;
+  hidetext = 'hide';
+  hide() {
+    this.click = !this.click;
+    if (this.click == false) {
+      this.hidetext = 'show';
+    } else if (this.click == true) {
+      this.hidetext = 'hide';
+    }
+    return this.click;
+  }
   rows = [
     {
-      "Name": "Shikha",
-      "Lastname": "Jaiswal",
-      "Zip": "433676",
-      "Phoneno": "7856029871"
+      Name: 'Shikha',
+      Lastname: 'Jaiswal',
+      Zip: '433676',
+      Phoneno: '7856029871',
     },
     {
-      "Name": "Abhishek",
-      "Lastname": "Maudiraj",
-      "Zip": "687366",
-      "Phoneno": "7856029871"
+      Name: 'Abhishek',
+      Lastname: 'Maudiraj',
+      Zip: '687366',
+      Phoneno: '7856029871',
     },
     {
-      "Name": "shambhavi",
-      "Lastname": "Duggal",
-      "Zip": "982565",
-      "Phoneno": "7856029871"
+      Name: 'shambhavi',
+      Lastname: 'Duggal',
+      Zip: '982565',
+      Phoneno: '7856029871',
     },
     {
-      "Name": "Joysi",
-      "Lastname": "Christien",
-      "Zip": "433676",
-      "Phoneno": "7856029871"
+      Name: 'Joysi',
+      Lastname: 'Christien',
+      Zip: '433676',
+      Phoneno: '7856029871',
     },
     {
-      "Name": "tom",
-      "Lastname": "claon",
-      "Zip": "433676",
-      "Phoneno": "7856029871"
-    }
+      Name: 'tom',
+      Lastname: 'claon',
+      Zip: '433676',
+      Phoneno: '7856029871',
+    },
   ];
 }
