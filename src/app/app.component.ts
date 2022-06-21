84ca373718e3a8ece1cea077b6df5ffa5dafdd96
NikitaSabne <nikitas@incubxperts.com>
2022-06-21 15:18:26 +0530

    dev-Nikita added age-validation and user-profile databinding Assignment project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'databinding';
+  age:any;
+  showAge: any;
+
+
+  ageCalculator() {
+    if (this.age) {
+      const convertAge = new Date(this.age);
+      const timeDiff = Math.abs(Date.now() - convertAge.getTime());
+      this.showAge = Math.floor((timeDiff / (1000 * 3600 * 24)) / 365);
+      console.log(this.showAge);
+
+    }
+  }
+
+  username:any;
+  email:any;
+  showuser() {
+    let username = (<HTMLInputElement>document.getElementById("username-in")).value;
+    let email = (<HTMLInputElement>document.getElementById("email-in")).value;
+  }
 }
